08af2d811d1b195f1a8986b25096d2b5d554da8b
unknown <fareedzeeshan868@gmail.com>
2025-07-10 19:16:16 +0500

    variables

---
+++ variables.php
@@ -0,0 +1,40 @@
+<?php 
+echo "this is variables";
+$name="zeeshan";
+$age="21";
+echo "<br>my name is $name and my age is $age<br>";
+$float=78.9;
+$floatN=-2.5;
+echo $float;
+echo "<br>";
+echo $floatN;
+echo "<br>";
+$tr=true;
+$fa=false;
+echo $tr;
+echo "<br>";
+echo $fa;
+
+// var_dump() for data type and value of variable
+var_dump($fa);
+echo "<br>";
+var_dump($float);
+//define array
+$arr=array("first","second","third");
+echo "<br>";
+var_dump($arr);
+echo "<br>";
+var_dump($arr[0]);
+echo "<br>";
+echo $arr[0]; //onyl value
+echo "<br>";
+var_dump($arr[1]);
+echo "<br>";
+//echo $arr[3];  give undefined as length is 2
+$nu=null;
+$nu1;
+var_dump($nu);
+echo "<br>";
+var_dump($nu1);
+
+?>
