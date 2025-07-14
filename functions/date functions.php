252631337083ff64ae41834061ffd27c846337b0
unknown <fareedzeeshan868@gmail.com>
2025-07-14 16:15:08 +0500

    get-post

--- functions/date functions.php
+++ functions/date functions.php
@@ -1,4 +1,6 @@
 /*
+https://www.php.net/manual/en/function.date.php 
+
 impotant functions 
 
 | **Function**                  | **Description**                                  | **Example Usage / Value**                                 |
@@ -22,24 +24,31 @@ day,date,year presentation
 | ------ | --------------------- | -------- | -------------------------- |
 | `Y`    | Full year (4 digits)  | `2025`   | **Y**ear full              |
 | `y`    | Short year (2 digits) | `25`     | **y**ear small             |
-| `m`    | Month (01)         | `07`     | **m**onth number           |
-| `n`    | Month (1, no 0)    | `7`      | **n**o leading zero month  |
+| `m`    | Month (01)            | `07`     | **m**onth number           |
+| `n`    | Month (1, no 0)       | `7`      | **n**o leading zero month  |
 | `F`    | Full month name       | `July`   | **F**ull month             |
 | `M`    | Short month name      | `Jul`    | **M**ini month             |
-| `d`    | Day (01)           | `13`     | **d**ay double-digit       |
-| `j`    | Day (1, no 0)      | `13`     | **j**ust the day (no zero) |
+| `d`    | Day (01)              | `13`     | **d**ay double-digit       |
+| `j`    | Day (1, no 0)         | `13`     | **j**ust the day (no zero) |
 | `D`    | Day short name        | `Sun`    | **D**ay 3-letter           |
 | `l`    | Full day name         | `Sunday` | "**L**ong day name"        |
+S - st, nd,rd,th suffix for the day  Works well with j
 
 
 time presentation
 
 | Format | Meaning         | Example | Memory Trick                    |
 | ------ | --------------- | ------- | ------------------------------- |
-| `H`    | 24-hour (00) | `14`    | **H**our military               |
-| `h`    | 12-hour (01) | `02`    | **h**our civil                  |
+| `H`    | 24-hour (00)    | `14`    | **H**our military               |
+| `h`    | 12-hour (01)    | `02`    | **h**our civil                  |
 | `i`    | Minutes (00-59) | `45`    | "**i** looks like minute stick" |
 | `s`    | Seconds (00-59) | `07`    | **s**econds                     |
 | `a`    | am or pm        | `pm`    | **a**m/pm                       |
 | `A`    | AM or PM        | `PM`    | **A**M/PM caps                  |
+
+
+"Y-m-d H:i:s" → most common full datetime format
+echo date("Y-m-d H:i:s");
+// Output: 2025-07-13 14:45:08
+
 
