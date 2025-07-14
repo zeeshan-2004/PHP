7f8df83ba54a043ab9909c686b4176dd59d61d8b
unknown <fareedzeeshan868@gmail.com>
2025-07-14 23:13:01 +0500

    scope of variables

--- variables/scope.php
+++ variables/scope.php
@@ -20,5 +20,63 @@ echo $a;
    echo $b; // 100
     echo "<br>";
    echo var_dump($GLOBALS);
-   echo var_dump($GLOBALS["a"]); // print a global
+   echo var_dump($GLOBALS["a"]); // print $a global
+
+    echo "<br>"; 
+   // global variables
+$fName="john";
+$lName="doe";
+function printN(){
+       global $fName, $lName;
+       echo  $fName ." ".$lName;
+    
+   
+};
+
+printN();
+
+   echo "<br>";
+   $x=($GLOBALS['lName']);
+echo $x;
+   echo "<br>";
+//global 
+$counter=0;
+function funinc(){
+ global  $counter;
+     echo  ++$counter;
+  
+}
+
+function douinc(){
+ global  $counter;
+     echo  $counter *=2;
+
+}
+ funinc(); // 1
+ echo "<br>";
+ douinc(); //2
+  echo "<br>";
+ echo $counter; //2
+  echo "<br>";
+
+ //$GLOBAL
+ $num=10;
+ function sq(){
+    // $a=$GLOBALS["num"];
+    // $a*=2;
+    //  echo $a;
+    $GLOBALS["num"]*=2;
+ }
+sq();
+echo $num;
+  echo "<br>";
+  $city="newYork";
+  $country="USA";
+  function location(){
+    $a=$GLOBALS['city'];
+    $b=$GLOBALS['country'];
+    $GLOBALS['location']=$a.",".$b;
+  }
+ location();
+ echo $location;
 ?>
