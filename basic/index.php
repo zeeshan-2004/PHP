bbfeb4cd570df816dfa6e14168aa89ae31f07db4
unknown <fareedzeeshan868@gmail.com>
2025-07-16 22:09:35 +0500

    basics

--- basic/index.php
+++ basic/index.php
@@ -1,13 +1,18 @@
 <?php
 echo"hello world";
 echo 100+6;
-?>
-
-<?php
-echo"hello world 2";
-
+echo "<br>";
 ?>
 <?php
-echo"hello world 2";
+echo"hello world 2"."how are you";
+echo "<br>";
+
+?>
+<?php
+echo"hello world 2","second string", "   ", " third string";
+echo "<br>";
+echo "<h1> this is "."heading</h1> <br> ";
+
+print "this is print ";
 
 ?>
