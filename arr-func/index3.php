5543cfc57632def02ce165feacbf36e8dcb8d41a
unknown <fareedzeeshan868@gmail.com>
2025-07-22 00:48:15 +0500

    till sorting

--- arr-func/index3.php
+++ arr-func/index3.php
@@ -1,6 +1,6 @@
 <?php
 //array_rand
-$arr=['a'=>'red','b'=>'green','c'=>'blue','d'=>'yellow'];
+$arr=['a'=>'apple','b'=>'green','c'=>'blue','e'=>'pink','d'=>'yellow'];
 $res=array_rand($arr);
 $res1=array_rand($arr,2);
 print_r($res);
@@ -35,7 +35,7 @@ $arr2=[$arr,'e'=>'purple','f'=>'yellow'];
 array_walk_recursive($arr2,'fun','has the value');
   echo "<br>";
 //arra_map
-$arr3=[2,4,5,6,7];
+$arr3=[2,4,5,6,3,1,8];
 function square($value){
  return $value*$value;
 }
@@ -56,6 +56,56 @@ print_r($res6);
   echo "<br>";
 $res7=array_map('upper',$arr);
 print_r($res7);
+  echo "<br>";
+//array_reduce
+function red($a,$b){
+  echo $a.'-'.$b;
+}
+function sum($a,$b){
+  //return $a+$b
+  $a+=$b; //  $a+$b equals => $a=$a+$b
+  return $a;
+}
+$res8=array_reduce($arr1,'red');
+print_r($res8);
+  echo "<br>";
+$res9=array_reduce($arr3,'sum'); //for * initail msut 1
+print_r($res9);
+  echo "<br>";
 
-
+//sort()
+sort($arr3);
+print_r($arr3);
+  echo "<br>";
+  rsort($arr3);
+print_r($arr3);
+ echo "<br>";
+ asort($arr);
+ print_r($arr);
+ echo "<br>";
+ arsort($arr);
+ print_r($arr);
+ echo "<br>";
+ ksort($arr);
+ print_r($arr);
+ echo "<br>";
+ krsort($arr);
+ print_r($arr);
+  echo "<br>";
+ $temp = array("temp15.txt","temp10.txt","temp1.txt","temp22.txt","temp2.txt");
+ $temp1 = array("Temp15.txt","Temp10.txt","temp1.txt","temp22.txt","temp2.txt");
+ natsort($temp);
+ print_r($temp);
+  echo "<br>";
+ natcasesort($temp1);
+ print_r($temp1);
+  echo "<br>";
+$arr4=['carrot','lichi','lemon','patato'];
+  array_multisort($arr1,$arr4);
+   print_r($arr1);
+  echo "<br>";
+   print_r($arr4);
+  echo "<br>";
+  $fin=array_reverse($arr4);
+   print_r($fin);
 ?>
---
+++ arr-func/practice2.php
@@ -0,0 +1,63 @@
+<?php
+$arr=['1'=>'ali','2'=>'zeeshan','3'=>'ahmad','4'=>'hassan'];
+$res=array_rand($arr,3);
+print_r($res);
+echo "<br>";
+$arr1=['first','second','third','fourth'];
+$res1=array_fill_keys($arr1,'read');
+print_r($res1);
+echo "<br>";
+$res2=array_fill(1,3,'0');
+print_r($res2);
+echo "<br>";
+$arr2=['a'=>20,'b'=>400,'c'=>50];
+$arr3=[$arr,'5'=>'murtaza'];
+function fun($a,$b){
+  if($a>100){
+    $a=0;
+    echo'price is'. $a.' '.$b;
+  }
+}
+function rec($a,$b){
+  if(is_string($a)){
+ echo strtoupper($a)." ";
+  }
+  
+}
+array_walk($arr2,'fun');
+print_r($arr2);
+echo "<br>";
+array_walk_recursive($arr3,'rec');
+print_r($arr3);
+echo "<br>";
+$arr4=[30,40,60];
+function cel($a){
+    $cel=($a*9/5)+32;
+    return $cel;
+}
+$res3=array_map('cel',$arr2);
+print_r($res3);
+echo "<br>";
+function sum($a,$b){
+  return $a+=$b;
+}
+$fin=array_reduce($arr4,'sum');
+print_r($fin,);
+echo "<br>";
+$arr5=[10,20.5,90,50.7,100];
+/*sort($arr5);
+print_r($arr5);
+echo "<br>";*/
+// rsort($arr5);
+// print_r($arr5);
+// asort($arr);
+// print_r($arr);
+// arsort($arr);
+// print_r($arr);
+$arr6=['1'=>'ali','4'=>'zeeshan','3'=>'ahmad','8'=>'hassan'];
+// ksort($arr6);
+// print_r($arr6);
+krsort($arr6);
+print_r($arr6);
+
+?>
